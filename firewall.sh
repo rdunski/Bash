cbbf2e384cb26c1c2e3b75b59f351bf94e1a600d
Robert Dunski <rdunski411@gmail.com>
2019-02-14 13:18:32 -0700

    added port and ip rules, help, and rule listing

--- firewall.sh
+++ firewall.sh
@@ -1,16 +1,47 @@
 #!/usr/bin/env bash
 
-ip=^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$
+ip="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
+port="^[0-9]{1,5}$"
 s="sudo"
-acceptArgs[0]="input"
-acceptArgs[1]="output"
-acceptArgs[2]="forward"
+
+acceptedOption[0]="delete"
+acceptedOption[1]="insert"
+acceptedOption[2]="list"
+acceptedOption[3]="add"
+
+acceptedPolicy[0]="drop"
+acceptedPolicy[1]="accept"
+
+acceptedChain[0]="input"
+acceptedChain[1]="output"
+acceptedChain[2]="forward"
+
 if [[ -z $1 ]]
   then
     echo "no args given"
     exit
 fi
 
+if [[ $1 == ${acceptedOption[2]} ]]
+  then
+    $s iptables -L
+    exit
+fi
+if [[ $1 == "help" ]]
+  then
+    echo -e "Usage: \t firewall help"
+    echo -e "\t firewall [policy] [ip] [chain]"
+    echo -e "\t firewall [policy] all [chain]"
+    echo -e "\t firewall flush [chain]"
+    echo -e "\t firewall flush all"
+    echo -e "\t firewall list\n"
+    echo -e "Policies:accept"
+    echo -e "\t drop\n"
+    echo -e "Chains:\t input"
+    echo -e "\t output"
+    echo -e "\t forward\n"
+    exit
+fi
 if [[ $1 == "flush" ]] && [[ -z $2 ]]
  then
   echo "did you mean 'flush all'? (y/n): "
@@ -49,49 +80,71 @@ elif [[ $1 == "flush" ]] && [[ $2 == "all" ]]
   exit
 fi
 
-if [[ $1 == "drop" ]]
- then
-  if [[ $2 =~ $ip ]]
-   then
-    for i in acceptArgs; do
-      if [[ $3 == $i ]]
-       then
-        $s iptables -A ${3^^} -s $2 -j ${1^^}
+if [[ $1 == ${acceptedOption[3]} ]]
+  then
+    if [[ $2 == "ping" ]]
+      then
+        for i in ${acceptedChain[*]}; do
+          if [[ $3 == $i ]]
+            then
+              $s iptables -p icmp --icmp-type any -A {$3^^}
+              exit
+          fi
+        done
+        echo "no valid chain specified"
         exit
-      fi
-    done
-  fi
-  if [[ $2 == "all" ]]
-   then
-    for i in acceptArgs; do
-      if [[ $3 == $i ]]
-       then
-        $s iptables -P ${3^^} ${1^^}
-        exit
-      fi
-    done
-  fi
+    fi
+    if [[ -z $2 ]]
+      then
+        echo "please specify a port number"
+        read pNum
+        echo "please specify a chain"
+        read chain
+        if [[ $pNum =~ $port ]]
+          then
+            if [[ $chain == "output" ]]
+              then
+                $s iptables -A ${chain^^} -p tcp --sport $pNum
+                exit
+            elif [[ $chain == "input" ]]
+              then
+                $s iptables -A ${chain^^} -p tcp --dport $pNum
+                exit
+            else
+              echo "not a valid chain"
+              exit
+            fi
+        else
+          echo "not a valid port"
+          exit
+        fi
+    fi
 fi
 
-if [[ $1 == "accept" ]]
- then
-  if [[ $2 =~ $ip ]]
+for a in ${acceptedPolicy[*]}; do
+  if [[ $1 == "drop" ]] || [[ $1 == "accept" ]]
    then
-    for i in acceptArgs; do
-      if [[ $3 == $i ]]
-       then
-        $s iptables -A ${3^^} -s $2 -j ${1^^}
-        exit
-      fi
-    done
+    if [[ $2 =~ $ip ]]
+     then
+      for i in ${acceptedChain[*]}; do
+        if [[ $3 == $i ]]
+         then
+          $s iptables -A ${3^^} -s $2 -j ${1^^}
+          exit
+        fi
+      done
+    fi
+    if [[ $2 == "all" ]]
+     then
+      for i in ${acceptedChain[*]}; do
+        if [[ $3 == $i ]]
+         then
+          $s iptables -P ${3^^} ${1^^}
+          exit
+        fi
+      done
+    fi
   fi
-  if [[ $2 == "all" ]]
-   then
-    for i in acceptArgs; do
-      if [[ $3 == $i ]]
-       then
-        $s iptables -P ${3^^} ${1^^}
-      fi
-    done
-  fi
-fi
+done
+
+echo "not a valid option"
