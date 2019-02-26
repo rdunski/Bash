86c9ab549979907e21971f20e074f16800192436
Robert Dunski <rdunski411@gmail.com>
2019-02-25 17:47:30 -0700

    added delete option

--- firewall.sh
+++ firewall.sh
@@ -2,6 +2,7 @@
 
 ip="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 port="^[0-9]{1,5}$"
+num=$port
 s="sudo"
 consoleChar="$"
 
@@ -9,7 +10,7 @@ if [[ $UID == "0" ]]; then
   consoleChar="#"
 fi
 
-pre="[Firewall]$consoleChar"
+pre="[Firewall]$consoleChar$command"
 
 acceptedOption[0]="delete"
 acceptedOption[1]="insert"
@@ -38,105 +39,81 @@ while(true); do
 #                                                             #
 ###############################################################
 
-#--------------------------Add--------------------------------#
-  if [[ $arg == ${acceptedOption[3]} ]]; then
-      echo "$pre what rule would you like to add?"
-      echo "accepted options are:"
-      echo -e "\t icmp (ping)"
-      echo -e "\t tcp"
-      echo -e -n "\t ip\n$pre " && read arg
-      if [[ $arg == "icmp" ]]; then
-        echo -n "$pre please specify a chain (Default=input): " && read arg
-
-          if [[ $arg == "input" ]] || [[ $arg == "" ]]; then
-            $s iptables -p icmp --icmp-type any -A INPUT
-
-          elif [[ $arg == "output" ]] || [[ $arg == "forward" ]]; then
-              $s iptables -p icmp --icmp-type any -A ${arg^^}
-              echo "$pre rule added successfully"
-
-          else
-            echo -n "$pre invalid chain, please try again"
-          fi
-
-      elif [[ $arg == "tcp" ]]; then
-          pnum=null
-          chain=null
-          echo "$pre please specify a port number" && echo -n "$pre " \
-            && read pNum
-          echo "$pre please specify a chain" && echo -n "$pre " \
-            && read chain
-
-          if [[ $pNum =~ $port ]]; then
-            if [[ $chain == "output" ]]; then
-              $s iptables -A ${chain^^} -p tcp --sport $pNum \
-                && echo "$pre rule added successfully"
-
-            elif [[ $chain == "input" ]]; then
-              $s iptables -A ${chain^^} -p tcp --dport $pNum \
-                && echo "$pre rule added successfully"
-
-            else
-              echo "$pre not a valid chain, please try again"
-            fi
-
-          else
-            echo "$pre not a valid port, please try again"
-          fi
-
-      elif [[ $arg == "ip" ]]; then
-        echo -n "$pre please enter an ip address (0.0.0.0-255.255.255.255): " \
-          && read address
-
-          if [[ $address =~ $ip ]]; then
-            echo -n "$pre drop or accept this ip? (drop/accept): " \
-              && read policy
-
-              if [[ $policy != "drop" ]] && [[ $policy != "accept" ]]; then
-                echo "$pre not a valid policy, please try again"
-                continue
-              fi
-
-            echo -n "$pre please specify a chain: " && read chain
-            found="false"
-
-              for i in ${acceptedChain[*]}; do
-                if [[ $chain == $i ]]; then
-                  $s iptables -A ${chain^^} -s $address -j ${policy^^} && echo "$pre rule added successfully"
-                  found="true"
-                fi
-              done
-
-              if [[ $found == "false" ]]; then
-                echo "$pre not a valid chain, please try again"
-              fi
-
-          else
-            echo "$pre not a valid ip address, please try again"
-          fi
-
-      else
-        echo "$pre not a valid option, please try agin"
-      fi
-  fi
 #--------------------------List-------------------------------#
-  if [[ $1 == "list" ]] || [[ $arg == "list" ]]; then
-    $s iptables -L
+  if [[ $1 == "list" ]]; then
+    if [[ -z $2 ]]; then
+      $s iptables -L
+
+    elif [[ ! $2 == "input" ]] && [[ ! $2 == "output" ]] \
+      && [[ ! $2 == "forward" ]]; then
+      if [[ $2 == "specs" ]]; then
+        $s iptables -S
+      elif [[ $2 == "nums" ]]; then
+        $s iptables -L --line-numbers
+      else
+        echo "$pre list option not valid"
+      fi
+    else
+      if [[ $3 == "specs" ]]; then
+        $s iptables -S ${2^^}
+      elif [[ $3 == "nums" ]]; then
+        $s iptables -L ${2^^} --line-numbers
+      else
+        echo "$pre list option not valid"
+      fi
+    fi
+  fi
+  if [[ $arg == "list" ]]; then
+    echo "$pre please select an option"
+    echo -e "\t1. List All"
+    echo -e "\t2. List All (Including Rule #s)"
+    echo -e "\t3. List All (Specifications)"
+    echo -e "\t4. List Chain"
+    echo -e "\t5. List Chain (Including Rule #s)"
+    echo -e "\t6. List Chain (Specifications)"
+    echo -n $pre" " && read arg
+    if [[ $arg == "1" ]]; then
+      $s iptables -L
+    elif [[ $arg == "2" ]]; then
+      $s iptables -L --line-numbers
+    elif [[ $arg == "3" ]]; then
+      $s iptables -S
+    elif [[ $arg == "4" ]] || [[ $arg == "5" ]] || [[ $arg == "6" ]]; then
+      echo "$pre please specify a chain"
+      echo -n $pre" " && read chain
+      if [[ $chain == "input" ]] || [[ $chain == "output" ]]; then
+        if [[ $arg == "4" ]]; then
+          $s iptables -L ${chain^^}
+        elif [[ $arg == "5" ]]; then
+          $s iptables -L ${chain^^} --line-numbers
+        elif [[ $arg == "6" ]]; then
+          $s iptables -S ${chain^^}
+        fi
+      else
+        echo "$pre not a valid chain, please try again"
+      fi
+    else
+      echo "$pre not a valid option, please try again"
+    fi
   fi
 
 #--------------------------Help-------------------------------#
   if [[ $1 == "help" ]] || [[ $arg == "help" ]]; then
     echo "Usage (from CLI):"
     echo -e "\t firewall help"
+    echo -e "\t firewall list {chain} {specs/nums}"
+    echo -e "\t firewall add [ping/tcp port #] [chain]"
+    echo -e "\t firewall delete [chain] [rule #]"
+    echo -e "\t firewall delete [chain] [specification]"
     echo -e "\t firewall [policy] [ip] [chain]"
     echo -e "\t firewall [policy] all [chain]"
     echo -e "\t firewall flush [chain]"
-    echo -e "\t firewall flush all"
-    echo -e "\t firewall list\n"
+    echo -e "\t firewall flush all\n"
 
     echo "Usage (running firewall.sh standalone):"
     echo -e "\t help"
     echo -e "\t add"
+    echo -e "\t delete"
     echo -e "\t list\n"
 
     echo "Policies:"
@@ -147,6 +124,138 @@ while(true); do
     echo -e "\t input"
     echo -e "\t output"
     echo -e "\t forward\n"
+  fi
+
+#--------------------------Add--------------------------------#
+  if [[ $arg == ${acceptedOption[3]} ]]; then
+    echo "$pre what rule would you like to add?"
+    echo "accepted options are:"
+    echo -e "\t icmp (ping)"
+    echo -e "\t tcp"
+    echo -e -n "\t ip\n$pre " && read arg
+
+    if [[ $arg == "icmp" ]]; then
+      echo -n "$pre please specify a chain (Default=input): " && read arg
+
+      if [[ $arg == "input" ]] || [[ $arg == "" ]]; then
+        $s iptables -p icmp --icmp-type any -A INPUT
+
+      elif [[ $arg == "output" ]] || [[ $arg == "forward" ]]; then
+        $s iptables -p icmp --icmp-type any -A ${arg^^}
+        echo "$pre rule added successfully"
+
+        else
+          echo -n "$pre invalid chain, please try again"
+        fi
+
+      elif [[ $arg == "tcp" ]]; then
+        pnum=null
+        chain=null
+
+        echo "$pre please specify a port number" && echo -n "$pre " \
+          && read pNum
+        echo "$pre please specify a chain" && echo -n "$pre " \
+          && read chain
+
+        if [[ $pNum =~ $port ]]; then
+
+          if [[ $chain == "output" ]]; then
+            $s iptables -A ${chain^^} -p tcp --sport $pNum \
+              && echo "$pre rule added successfully"
+
+          elif [[ $chain == "input" ]]; then
+            $s iptables -A ${chain^^} -p tcp --dport $pNum \
+              && echo "$pre rule added successfully"
+
+          else
+            echo "$pre not a valid chain, please try again"
+          fi
+
+        else
+          echo "$pre not a valid port, please try again"
+        fi
+
+      elif [[ $arg == "ip" ]]; then
+        echo -n "$pre please enter an ip address (0.0.0.0-255.255.255.255): " \
+          && read address
+
+        if [[ $address =~ $ip ]]; then
+          echo -n "$pre drop or accept this ip? (drop/accept): " \
+            && read policy
+
+          if [[ $policy != "drop" ]] && [[ $policy != "accept" ]]; then
+            echo "$pre not a valid policy, please try again"
+            continue
+          fi
+
+          echo -n "$pre please specify a chain: " && read chain
+          found="false"
+
+          for i in ${acceptedChain[*]}; do
+            if [[ $chain == $i ]]; then
+              $s iptables -A ${chain^^} -s $address -j ${policy^^} && echo "$pre rule added successfully"
+              found="true"
+            fi
+          done
+
+          if [[ $found == "false" ]]; then
+            echo "$pre not a valid chain, please try again"
+          fi
+
+        else
+          echo "$pre not a valid ip address, please try again"
+        fi
+
+      else
+        echo "$pre not a valid option, please try agin"
+      fi
+  fi
+
+#-------------------------Delete------------------------------#
+  if [[ $arg == "delete" ]]; then
+    echo "$pre please choose an option"
+    echo -e "\t1. Rule Number"
+    echo -e "\t2. Rule Specification"
+    echo -n $pre" " && read arg
+
+    if [[ $arg == "1" ]]; then
+      echo "$pre please specify a chain"
+      echo -n $pre" " && read arg
+
+      if [[ $arg == "input" ]] || [[ $arg == "output" ]]; then
+        $s iptables -L ${arg^^} --line-numbers
+        echo "$pre please specify a rule number"
+        echo -n $pre" " && read rule
+
+        if [[ $rule =~ $num ]]; then
+          $s iptables -D ${arg^^} $rule && echo "$pre rule deleted" \
+            || echo "$pre rule not deleted, please try again"
+        fi
+
+      fi
+
+    elif [[ $arg == "2" ]]; then
+      echo "$pre please specify a chain"
+      echo -n $pre" " && read arg
+
+      if [[ $arg == "input" ]] || [[ $arg == "output" ]]; then
+        $s iptables -S ${arg^^}
+
+        echo -e "$pre please type in the specification to delete without the -A\n
+          (rules that don't have -A at the beginning are not valid for deletion)\n
+          ex. specification: -A INPUT -p tcp --sport 80 \n
+          \tinput: INPUT -p tcp --sport 80"
+        echo -n $pre" " && read rule
+
+        $s iptables -D ${arg^^} $rule && echo "$pre rule deleted" \
+          || echo "$pre rule not deleted, please make sure the specification \
+            exists and is typed in correctly"
+      fi
+
+    else
+      echo "$pre not a valid option, please try again"
+    fi
+
   fi
 
 #-------------------------Flush-------------------------------#
@@ -213,6 +322,20 @@ while(true); do
     fi
   fi
 
+#-----------Delete->Chain->Rule Num/Specification-------------#
+  if [[ $1 == "delete" ]]; then
+    if [[ $2 == "input" ]] || [[ $2 == "output" ]]; then
+      if [[ -z $3 ]]; then
+        echo "$pre no rule number or specification given"
+      else
+        $s iptables -D ${2^^} ${3^^} && echo "$pre rule deleted" || \
+          echo "rule not deleted, make sure the rule exists"
+      fi
+    else
+      echo "$pre not a valid chain"
+    fi
+  fi
+
 #-----------------Policy->IP/All->Chain-----------------------#
   if [[ $1 == "drop" ]] || [[ $1 == "accept" ]]; then
     if [[ $2 =~ $ip ]]; then
@@ -229,11 +352,9 @@ while(true); do
       if [[ $2 == "all" ]]; then
 
         for i in ${acceptedChain[*]}; do
-
           if [[ $3 == $i ]]; then
             $s iptables -P ${3^^} ${1^^}
           fi
-
         done
       fi
     fi
