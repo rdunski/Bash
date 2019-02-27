4ea88303f29bc9a913dca413c81387d3bcd9971e
Robert Dunski <rdunski411@gmail.com>
2019-02-27 13:57:53 -0700

    finished for now, looking into how to make program run globally

--- firewall.sh
+++ firewall.sh
@@ -6,10 +6,44 @@ num=$port
 s="sudo"
 consoleChar="$"
 
+#---------------------HTML Formatting-------------------------#
+printhtml() {
+  echo -e "<!DOCTYPE html>\n" \
+  "<html>\n" \
+  "<body style=background-color:#63CCCA>\n" \
+  "<p>\n" > report.html
+#--------------Take the iptables command given by arguments
+#--------------In this case, all the arguments given to the function
+#--------------is the whole command (hence $*) and use awk to parse the
+#--------------output and format it into HTML
+#--------------(I attempted to keep the long line below column 65, but awk
+#--------------doesn't like breaks, tabs, or \ escape characters)
+  $* | awk '/Chain/ {
+    print "<div style=display:block;text-align:center;width:auto;margin:auto;max-width:800px;background-color:#00A3BB;border-radius:4px>";
+    print "<h1 style=color:#000033>";
+    print;
+    print "</h1>"; next }
+    /target/ {
+    print "<h2 style=color:#black>";
+    print;
+    print "</h2>"; next }
+    // {
+    print "<p style=color:#EEEEFF align=center>";
+    print;
+    print "</p>";
+    print "</div>" }' >> report.html
+  echo -e "\n" \
+  "</p>\n" \
+  "</body>\n" \
+  "</html>" >> report.html
+}
+
+#--------------If user is root, change the $ to #
 if [[ $UID == "0" ]]; then
   consoleChar="#"
 fi
 
+#--------------Fancy prefix for interactive mode
 pre="[Firewall]$consoleChar$command"
 
 acceptedOption[0]="delete"
@@ -27,7 +61,11 @@ acceptedChain[2]="forward"
 interact="no"
 
 if [[ -z $1 ]]; then
-    echo "$pre Welcome to KomodoWall, type 'help' for a list of commands"
+    echo "#############################################"
+    echo "#        Welcome to the Firewall!           #"
+    echo "#       Created by:  Robert Dunski          #"
+    echo "#############################################"
+    echo "-----type 'help' for a list of commands------"
     interact="yes"
 fi
 
@@ -99,9 +137,11 @@ while(true); do
 
 #--------------------------Help-------------------------------#
   if [[ $1 == "help" ]] || [[ $arg == "help" ]]; then
+    echo "{} means optional, [] means required"
     echo "Usage (from CLI):"
     echo -e "\t firewall help"
     echo -e "\t firewall list {chain} {specs/nums}"
+    echo -e "\t firewall report {specs/nums}"
     echo -e "\t firewall add [ping/tcp port #] [chain]"
     echo -e "\t firewall delete [chain] [rule #]"
     echo -e "\t firewall delete [chain] [specification]"
@@ -112,20 +152,41 @@ while(true); do
 
     echo "Usage (running firewall.sh standalone):"
     echo -e "\t help"
+    echo -e "\t list"
+    echo -e "\t report"
     echo -e "\t add"
-    echo -e "\t delete"
-    echo -e "\t list\n"
+    echo -e "\t delete\n"
 
     echo "Policies:"
     echo -e "\t accept"
     echo -e "\t drop\n"
 
-    echo -e "Chains:"
+    echo "Chains:"
     echo -e "\t input"
     echo -e "\t output"
     echo -e "\t forward\n"
   fi
 
+#-------------------------Report------------------------------#
+if [[ $arg == "report" ]]; then
+  echo "$pre please select an option"
+  echo -e "\t1. Report"
+  echo -e "\t2. Report w/ Rule Numbers"
+  echo -e "\t3. Report Specificiations" && echo -n "$pre " && read arg
+  if [[ $arg == "1" ]]; then
+    command="$s iptables -L"
+    printhtml $command && echo "$pre report created at report.html"
+  elif [[ $arg == "2" ]]; then
+    command="$s iptables -L --line-numbers" && \
+      echo "$pre report created at report.html"
+    printhtml $command
+  elif [[ $arg == "3" ]]; then
+    command="$s iptables -S" && echo "$pre report created at report.html"
+    printhtml $command
+  else
+    echo "$pre not a valid option, please try again"
+  fi
+fi
 #--------------------------Add--------------------------------#
   if [[ $arg == ${acceptedOption[3]} ]]; then
     echo "$pre what rule would you like to add?"
@@ -358,6 +419,21 @@ while(true); do
         done
       fi
     fi
+
+#---------------------Report->Specs/Nums----------------------#
+if [[ $1 == "report" ]]; then
+  if [[ -z $2 ]]; then
+    command="$s iptables -L"
+    printhtml $command
+  elif [[ $2 == "nums" ]]; then
+    command="$s iptables -L --line-numbers"
+    printhtml $command
+  elif [[ $2 == "specs" ]]; then
+    command="$s iptables -S"
+    printhtml $command
+  fi
+fi
+
 
 #--------------------If not interactive-----------------------#
   if [[ $interact == "no" ]]; then
